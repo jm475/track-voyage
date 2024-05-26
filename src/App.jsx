608a7b86143cbd71b4f52141d3d1abad3754dbf4
John <johnmacdonald789@gmail.com>
2024-05-26 17:34:37 +1200

    Inital commit - When page first loads It will prompt the user to connect spotify to the application - After user agrees page will display users top 50 songs in the past month - NOTE: When refreshing the page the top 50 songs wil not display

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import {trackNames} from './APIRequest'
+
+function App() {
+  const [trackNamesHook, setTrackNamesHook] = useState([])
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        localStorage.setItem('trackNames', JSON.stringify(trackNames));
+        setTrackNamesHook(trackNames); // Set track names to state
+      } catch (error) {
+        console.error('Error fetching track names:', error);
+      }
+    };
+    fetchData();
+  }, []); // Empty dependency array ensures this effect runs only once after the component mounts
+
+
+
+  return (
+    <div id="app">
+    <h2>Track Names:</h2>
+      <ol type="1">
+        {trackNamesHook.map((trackName, index) => (
+          <li key={index}> {trackName}</li>
+        ))}
+      </ol>
+    </div>
+  )
+}
+
+export default App
